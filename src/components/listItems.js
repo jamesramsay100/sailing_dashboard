fddc307eb3a789be6e1d9c6771e89410aa3d7a5a
James Ramsay <james@lytt.tech>
2021-03-13 12:07:16 +0000

    added upload button

--- src/components/listItems.js
+++ src/components/listItems.js
@@ -7,6 +7,13 @@ import DashboardIcon from "@material-ui/icons/Dashboard";
 import BarChartIcon from "@material-ui/icons/BarChart";
 import LayersIcon from "@material-ui/icons/Layers";
 import DirectionsBoatIcon from "@material-ui/icons/DirectionsBoat";
+import CloudUploadIcon from "@material-ui/icons/CloudUpload";
+import Button from "@material-ui/core/Button";
+import { styled } from "@material-ui/core/styles";
+
+const Input = styled("input")({
+  display: "none",
+});
 
 export const mainListItems = (
   <div>
@@ -51,6 +58,29 @@ export const secondaryListItems = (
         <DirectionsBoatIcon />
       </ListItemIcon>
       <ListItemText primary="Boat 3" />
+    </ListItem>
+  </div>
+);
+
+export const thirdListItems = (
+  <div>
+    <ListItem button>
+      <ListItemIcon>
+        <CloudUploadIcon />
+      </ListItemIcon>
+      <ListItemText>
+        <Input
+          accept="image/*"
+          id="contained-button-file"
+          multiple
+          type="file"
+        />
+        <label htmlFor="contained-button-file">
+          <Button variant="contained" component="span">
+            Upload
+          </Button>
+        </label>
+      </ListItemText>
     </ListItem>
   </div>
 );
